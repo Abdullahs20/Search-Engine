9853546f02bfed39d1393626303698a8774f3c40
IBRAHIMALMUHSUN <ibra108him512@gmail.com>
2024-11-14 16:02:00 +0300

    Update Node.java

--- Node.java
+++ Node.java
@@ -2,6 +2,10 @@
 public class Node<T> {
     public T Data;
     public Node<T> Next;
+    public Node() {
+		data = null;
+		next = null;
+	}
     public Node (T val) {
         Data=val;
         Next=null;
