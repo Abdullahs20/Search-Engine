304ee2b70910825f26f3ce71bc5d0428e53eff2a
Abdullahs20 <abdullahalaskar1993@gmail.com>
2024-11-16 23:11:21 +0300

    Add class QueryP And make some changes in class Main to test it

--- Main.java
+++ Main.java
@@ -2,93 +2,121 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 public class Main {
-        LinkedList <String> StopWord;
-        Index Index; //LL and Doc
-        InvertedIndex Inverted; //LL of word
-        InvertedindexBST InvertedBST;
-        public Main(){
-            StopWord = new LinkedList<>();
-            Index= new Index() ;
-            Inverted =new InvertedIndex() ;
-            InvertedBST =new InvertedindexBST();
+    LinkedList <String> StopWord;
+    Index Index; //LL and Doc
+    InvertedIndex Inverted; //LL of word
+    InvertedindexBST InvertedBST;
+    public Main(){
+        StopWord = new LinkedList<>();
+        Index= new Index() ;
+        Inverted =new InvertedIndex() ;
+        InvertedBST =new InvertedindexBST();
+    }
+    public void LoadStopWords (String fileName){ //Load the stop words to read it
+        try {
+            File f=new File (fileName);
+            Scanner s=new Scanner (f);
+            while (s.hasNextLine()) {
+                String line=s.nextLine();
+                StopWord.insert(line);
+            }
+        }catch(IOException e) {
+            e.printStackTrace();
         }
-        public void LoadStopWords (String fileName){ //Load the stop words to read it
-            try {
-                File f=new File (fileName);
-                Scanner s=new Scanner (f);
-                while (s.hasNextLine()) {
-                    String line=s.nextLine();
-                    StopWord.insert(line);
+    }
+    public void LoadAllDoc (String fileName) {//Load the Doc to read it
+        String line = null;
+        try {
+            File f = new File(fileName);
+            Scanner s = new Scanner(f);
+            s.nextLine(); //Skip first line in Excel
+            while (s.hasNextLine()) {
+                line = s.nextLine();
+                if (line.trim().length() < 3) {
+                    System.out.println("Empty line Found");
+                    break;
                 }
-            }catch(IOException e) {
-                e.printStackTrace();
+                String x = line.substring(0, line.indexOf(","));
+                int ID = Integer.parseInt(x.trim());
+                String Content = line.substring(line.indexOf(",") + 1);
+                LinkedList<String> WordInDoc = LLOfWordInIndexOfInvertedIndex(Content, ID);
+                Index.Add_Doc(new Document(ID, WordInDoc,Content)); //All word and with their Doc
+            }
+        } catch(Exception e) {
+            System.out.println(e);
+        }
+    }
+    public LinkedList<String> LLOfWordInIndexOfInvertedIndex (String Content, int ID){
+        LinkedList<String> WordsInDoc = new LinkedList<String>() ;
+        IndexOfInvertedIndex(Content, WordsInDoc, ID) ; //line 52
+        return WordsInDoc;
+    }
+    public void IndexOfInvertedIndex (String Content, LinkedList<String>WordsInDoc, int ID) {
+        Content = Content.toLowerCase().replaceAll("[^a-zA-Z0-9 ]", ""); // make all letter lowercases
+        String[] Token = Content.split ("\\s+");
+        for (String W : Token) {
+            if (!IsExistInStopWord(W)) { //Search !!!!
+                WordsInDoc.insert(W);
+                Inverted.add(W, ID);
+                InvertedBST.add(W, ID);
             }
         }
-        public void LoadAllDoc (String fileName) {//Load the Doc to read it
-            String line = null;
-            try {
-                File f = new File(fileName);
-                Scanner s = new Scanner(f);
-                s.nextLine(); //Skip first line in Excel
-                while (s.hasNextLine()) {
-                    line = s.nextLine();
-                    if (line.trim().length() < 3) {
-                        System.out.println("Empty line Found");
-                        break;
-                    }
-                    String x = line.substring(0, line.indexOf(","));
-                    int ID = Integer.parseInt(x.trim());
-                    String Content = line.substring(line.indexOf(",") + 1);
-                    LinkedList<String> WordInDoc = LLOfWordInIndexOfInvertedIndex(Content, ID);
-                    Index.Add_Doc(new Document(ID, WordInDoc,Content)); //All word and with their Doc
-                }
-            } catch(Exception e) {
-                System.out.println(e);
-            }
-        }
-        public LinkedList<String> LLOfWordInIndexOfInvertedIndex (String Content, int ID){
-            LinkedList<String> WordsInDoc = new LinkedList<String>() ;
-            IndexOfInvertedIndex(Content, WordsInDoc, ID) ; //line 52
-            return WordsInDoc;
-        }
-        public void IndexOfInvertedIndex (String Content, LinkedList<String>WordsInDoc, int ID) {
-            Content = Content.toLowerCase().replaceAll("[^a-zA-Z0-9 ]", ""); // make all letter lowercases
-            String[] Token = Content.split ("\\s+");
-            for (String W : Token) {
-                if (!IsExistInStopWord(W)) { //Search !!!!
-                    WordsInDoc.insert(W);
-                    Inverted.add(W, ID);
-                    InvertedBST.add(W, ID);
-                }
-            }
-        }
-        public boolean IsExistInStopWord (String Word) { //Search if the word Exist or not
-            if (StopWord==null || StopWord.empty())
-                return false;
-            StopWord.findFirst();
-            while (!StopWord.last()) {
-                if (StopWord.retrieve().equals(Word)) {
-                    return true;
-                }
-                StopWord.findNext();
-            }
-            if(StopWord.retrieve().equals(Word))
-                return true;
-
+    }
+    public boolean IsExistInStopWord (String Word) { //Search if the word Exist or not
+        if (StopWord==null || StopWord.empty())
             return false;
+        StopWord.findFirst();
+        while (!StopWord.last()) {
+            if (StopWord.retrieve().equals(Word)) {
+                return true;
+            }
+            StopWord.findNext();
         }
-        public void LoadFiles (String StopFile,String DocumentFile) { //Load the stop words and the doc file
-            LoadStopWords(StopFile); //Focus stop word text should be before the Doc file
-            LoadAllDoc(DocumentFile);
-        }
-    public static void main (String [] args) {
-            Main d = new Main();
-            d.LoadFiles("stop.txt", "dataset.csv");
-//            d.Index.DisplayDoc();
-            System.out.println("\n======================");
-//            d.Inverted.display_inverted_index(); //from class InvertedIndex*
-        System.out.println("\n======================");
-        d.InvertedBST.DisplayInvertedIndex();
-    }
-    }
+        if(StopWord.retrieve().equals(Word))
+            return true;
 
+        return false;
+    }
+    public void LoadFiles (String StopFile,String DocumentFile) { //Load the stop words and the doc file
+        LoadStopWords(StopFile); //Focus stop word text should be before the Doc file
+        LoadAllDoc(DocumentFile);
+    }
+    public void DisplayDocWithID(LinkedList<Integer> Id){
+        if(Id.empty()){
+            System.out.println("No Document Found");
+            return;
+        }
+        Id.findFirst();
+        while (!Id.last()) {
+            Document D=Index.GetDocByID(Id.retrieve());
+            if(D!=null){
+                System.out.println("\nDocument "+D.ID +": "+D.Content);
+//                System.out.println("\n");
+            }
+            Id.findNext();
+        }
+        Document D=Index.GetDocByID(Id.retrieve());
+        if(D!=null){
+            System.out.println("\nDocument "+D.ID +": "+D.Content);
+//            System.out.println("\n");
+        }
+        System.out.println("===================");
+
+    }
+    public static void main (String [] args) {
+        Main d = new Main();
+        d.LoadFiles("stop.txt", "dataset.csv");
+//            d.Index.DisplayDoc();
+        System.out.println("\n======================");
+//          d.Inverted.display_inverted_index(); //from class InvertedIndex*
+        System.out.println("\n======================");
+//        d.InvertedBST.DisplayInvertedIndex();
+//         d.Inverted.display_inverted_index();
+
+         QueryP q=new QueryP(d.Inverted);
+        LinkedList l=QueryP.ORQuery("businessORworld");
+        d.DisplayDocWithID(l);
+
+
+    }
+}
---
+++ QueryP.java
@@ -0,0 +1,133 @@
+public class QueryP {
+    static InvertedIndex Inverted;
+    public QueryP(InvertedIndex Inverted) {
+        this.Inverted = Inverted;
+    }
+    public static LinkedList<Integer>AndQuery(String Query) { //7
+        LinkedList<Integer> X = new LinkedList<>();//Two LL to Work With it
+        LinkedList<Integer> Y = new LinkedList<>();
+        String[] T = Query.split("AND");//use split in array
+        if (T.length == 0)
+            return X;
+        boolean Found = Inverted.search_inverted_word_(T[0].trim().toLowerCase()); //search_inverted_word_ you find it in class InvertedIndex , also when we split the new LL will be empty
+        if (Found) {
+            X = Inverted.WordList.retrieve().DOC_ID;
+        }
+        for (int i = 1; i < T.length; i++) {
+            Found = Inverted.search_inverted_word_(T[i].trim().toLowerCase());
+            if (Found)
+                Y = Inverted.WordList.retrieve().DOC_ID;
+            X = AndQuery(X, Y);
+        }
+        return X;
+    }
+    public static LinkedList<Integer> AndQuery(LinkedList<Integer> A, LinkedList<Integer> B){ //use LL to make split
+        LinkedList<Integer> Result = new LinkedList<>();
+        if (A.empty() || B.empty())
+            return Result;
+        A.findFirst();
+        while (true) {
+            boolean Found = ExistInRes(Result, A.retrieve()); //if you didn't find it you will use the code in 35 to 47 line
+            if (!Found) {
+                B.findFirst();
+                while (true) {
+                    if (B.retrieve().equals(A.retrieve())) {
+                        Result.insert(A.retrieve());
+                        break;
+                    }
+                    if (!B.last())
+                        B.findNext();
+                    else
+                        break;
+                }
+            }
+            if (!A.last())
+                A.findNext();
+            else
+                break;
+        }
+        return Result;
+    }
+    public static LinkedList<Integer> ORQuery(String Query) {
+        LinkedList<Integer> A = new LinkedList<Integer>();
+        LinkedList<Integer> B = new LinkedList<Integer>();
+        String [] T = Query.split("OR");
+        if (T.length == 0) return A;
+        boolean Found = Inverted.search_inverted_word_(T[0].trim().toLowerCase());
+        if (Found) {
+            A = Inverted.WordList.retrieve().DOC_ID;
+        }
+        for (int i = 1; i < T.length; i++) {
+            Found = Inverted.search_inverted_word_(T[i].trim().toLowerCase());
+            if (Found) {
+                B = Inverted.WordList.retrieve().DOC_ID;
+            }
+            A = ORQuery(A, B);
+        }
+        return A;
+    }
+    public static LinkedList<Integer> ORQuery(LinkedList<Integer> A, LinkedList<Integer> B) {
+        LinkedList<Integer> Result = new LinkedList<Integer>();
+        if (A.empty() && B.empty())
+            return Result;
+        A.findFirst();
+        while (!A.empty()) { //we be false when it's empty
+            boolean Found = ExistInRes(Result, A.retrieve());
+            if (!Found) {
+                Result.insert(A.retrieve());
+            }
+            if (!A.last())
+                A.findNext();
+            else
+                break;
+        }
+        B.findFirst();
+        while (!B.empty()) {
+            boolean Found = ExistInRes(Result, B.retrieve());
+            if (!Found) {
+                Result.insert(B.retrieve());
+            }
+            if (!B.last())
+                B.findNext();
+            else
+                break;
+        }
+        return Result;
+    }
+    public static LinkedList<Integer>MixedQuery(String Query) {
+        LinkedList<Integer>A=new LinkedList<Integer>();
+        LinkedList<Integer>B=new LinkedList<Integer>();
+        if(Query.length()==0)
+            return A;
+        String []M= Query.split("OR");
+        A=AndQuery(M[0]);
+        for (int i = 1; i < M.length; i++) {
+            B=AndQuery(M[i]);
+            A=ORQuery(A, B);
+        }
+        return A;
+    }
+
+    public static boolean ExistInRes(LinkedList<Integer> Result , Integer ID) {
+        if(Result.empty())
+            return false;
+        Result.findFirst();
+        while (!Result.last()){
+            if(Result.retrieve().equals(ID)){
+                return true;
+            }
+            Result.findNext();
+        }
+        if(Result.retrieve().equals(ID)){
+            return true;}
+
+        return false;
+    }
+}
+
+
+
+
+
+
+
