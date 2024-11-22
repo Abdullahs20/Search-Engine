194eed46edeb42bc2e7f7cd29d61b6d42e6612a8
Abdullahs20 <149415568+Abdullahs20@users.noreply.github.com>
2024-11-22 03:28:48 +0300

    Adding class QueryIndex

---
+++ QueryIndex.java
@@ -0,0 +1,121 @@
+public class QueryIndex {
+    //this is the worst because will move in every document also inside it we will search in almost in every words BigO(n*words)
+static Index Index;
+public QueryIndex(Index index) {
+    Index = index;
+}
+public static LinkedList<Integer> AndQuery(String Query) {
+    LinkedList<Integer> Result = new LinkedList<>();
+    if (Query.isEmpty())
+        return Result;
+    String[] terms = Query.split("AND");
+    if (terms.length == 0)
+        return Result;
+    Result = Index.getDocumentTerm(terms[0].trim().toLowerCase());// we use this getDocumentTerm instead of searching because the previous search in QueryP and QueryPBST  it's return a boolean
+    for (int i = 1; i < terms.length; i++) {
+        LinkedList<Integer> Result1 = Index.getDocumentTerm(terms[i].trim().toLowerCase());
+        Result = AndQuery(Result, Result1);
+        if (Result.empty()) {
+            break;
+        }
+    }
+    return Result;
+}
+    public static LinkedList<Integer> AndQuery(LinkedList<Integer> A, LinkedList<Integer> B){ //use LL to make split
+        LinkedList<Integer> Result = new LinkedList<>();
+        if (A.empty() || B.empty())
+            return Result;
+        A.findFirst();
+        while (true) {
+            boolean Found = ExistInRes(Result, A.retrieve()); //if you didn't find it you will use the code in 35 to 47 line
+            if (!Found) {
+                B.findFirst();
+                while (true) {
+                    if (B.retrieve().equals(A.retrieve())) {
+                        Result.insert(A.retrieve());
+                        break;
+                    }
+                    if (!B.last())
+                        B.findNext();
+                    else
+                        break;
+                }
+            }
+            if (!A.last())
+                A.findNext();
+            else
+                break;
+        }
+        return Result;
+    }
+    public static LinkedList<Integer> ORQuery(String Query) {
+        LinkedList<Integer> A = new LinkedList<Integer>();
+        LinkedList<Integer> B = new LinkedList<Integer>();
+        String [] T = Query.split("OR");
+        if (T.length == 0) return A;
+        A=Index.getDocumentTerm(T[0].trim().toLowerCase());
+        for (int i = 0; i < T.length; i++) {
+            B= Index.getDocumentTerm(T[i].trim().toLowerCase());
+            A=ORQuery(A, B); //check
+        }
+        return A;
+    }
+    public static LinkedList<Integer> ORQuery(LinkedList<Integer> A, LinkedList<Integer> B) {
+        LinkedList<Integer> Result = new LinkedList<Integer>();
+        if (A.empty() && B.empty())
+            return Result;
+        A.findFirst();
+        while (!A.empty()) { //we be false when it's empty
+            boolean Found = ExistInRes(Result, A.retrieve());
+            if (!Found) {
+                Result.insert(A.retrieve());
+            }
+            if (!A.last())
+                A.findNext();
+            else
+                break;
+        }
+        B.findFirst();
+        while (!B.empty()) {
+            boolean Found = ExistInRes(Result, B.retrieve());
+            if (!Found) {
+                Result.insert(B.retrieve());
+            }
+            if (!B.last())
+                B.findNext();
+            else
+                break;
+        }
+        return Result;
+    }
+    public static LinkedList<Integer>MixedQuery(String Query) {
+        LinkedList<Integer>A=new LinkedList<Integer>();
+        LinkedList<Integer>B=new LinkedList<Integer>();
+        if(Query.length()==0)
+            return A;
+        String []M= Query.split("OR");
+        A=AndQuery(M[0]);
+        for (int i = 1; i < M.length; i++) {
+            B=AndQuery(M[i]);
+            A=ORQuery(A, B);
+        }
+        return A;
+    }
+    public static boolean ExistInRes(LinkedList<Integer> Result , Integer ID) {
+        if(Result.empty())
+            return false;
+        Result.findFirst();
+        while (!Result.last()){
+            if(Result.retrieve().equals(ID)){
+                return true;
+            }
+            Result.findNext();
+        }
+        if(Result.retrieve().equals(ID)){
+            return true;}
+
+        return false;
+    }
+
+
+}
